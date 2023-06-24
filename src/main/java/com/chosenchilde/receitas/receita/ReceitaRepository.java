0cd79c6ffc1ca12b7124b9c01e5d93d29584ebec
Chosen Child <edencyberspace@gmail.com>
2023-06-23 21:14:03 -0300

    query

--- src/main/java/com/chosenchilde/receitas/receita/ReceitaRepository.java
+++ src/main/java/com/chosenchilde/receitas/receita/ReceitaRepository.java
@@ -12,5 +12,6 @@ public interface ReceitaRepository extends JpaRepository<Receita, Long> {
 
 	@Query(value = "SELECT * FROM RECEITA WHERE UPPER (NAME) LIKE UPPER (concat('%', :q1, '%'))  OR UPPER (CONTENT) LIKE  UPPER (concat('%', :q1, '%'))  OR  UPPER (INGREDIENTS) LIKE  UPPER (concat('%', :q1, '%'))  AND STATUS = 'on'", nativeQuery = true)
 	List<Receita> buscaReceita(@Param("q1") String q1);
-		
+	
+	/* SELECT * FROM RECEITA  INNER JOIN USUARIO ON RECEITA.AUTHOR = USUARIO.ID */
 }
